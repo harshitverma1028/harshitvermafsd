90504a257048e9e5acbca7d04a2f3a7c16e8346b
harshitverma1028 <harshitverma8171@gmail.com>
2025-10-29 10:11:03 +0530

    react

---
+++ React/harshit/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Card.css'
+function Card() {
+  return (
+    <div id='hv'>
+        <img src="https://images.unsplash.com/photo-1761405378543-e74453424152?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8&auto=format&fit=crop&q=60&w=600" alt="image" height={100} width={100}/>
+        <h1>Harshit verma</h1>
+    </div>
+  )
+}
+
+export default Card
---
+++ React/harshit/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './nav.css'
+function Nav() {
+  return (
+    <div id='nav'>
+        <a href="">Home</a>
+        <a href="">About us</a>
+        <a href="">Help</a>
+        <a href="">Contact us</a>
+        <a href="">Login</a>
+    </div>
+  )
+}
+
+export default Nav
