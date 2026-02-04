c3d1feb31d5d6ab898c696b6893f159fc5831347
harshitverma1028 <harshitverma8171@gmail.com>
2026-02-04 10:12:07 +0530

    commit

--- Node/index.js
+++ Node/index.js
@@ -1,10 +1,23 @@
-import http from "http"
-
-const app = http.createServer((req,res)=>{
-    console.log('Server started.....')
-    res.end('This is my server')
-})
-
-app.listen(8000,(req,res)=>{
-    console.log('Server is running on port 8000.....')
+const http=require('http');
+const myserver=http.createServer((req,res)=>{
+    if(req.url=='/'){
+        res.end('welcome to home page');
+    }
+    else if(req.url=='/about'){
+        res.end(`<h1>abes engineering college</h1>
+            <img src="https://unsplash.com/photos/a-dark-grey-sports-car-with-red-wheels-hSCrpBasAss">
+        `);
+    }
+    else if(req.url=='/contact us'){
+        res.end('1234567890');
+    }
+    else if(req.url=='/class'){
+        res.end('I.T-A');
+    }
+    else{
+        res.end('404 page not found');
+    }
+});
+myserver.listen(8000,(req,res)=>{
+    console.log("server is running on port 8000");
 })
